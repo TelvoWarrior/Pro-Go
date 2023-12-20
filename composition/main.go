397874c4b458388d5d79f86b69ad5efb97ada38f
Telvo <telvowarrior@gmail.com>
2023-12-20 15:29:20 +0300

    add specialdeal.go

--- composition/main.go
+++ composition/main.go
@@ -24,4 +24,24 @@ func main() {
 		fmt.Println("Conventional:", b.Product.Name, "Direct:", b.Name)
 		fmt.Println("Boat:", b.Name, "Price:", b.Price(0.2))
 	}
+
+	rentals := []*store.RentalBoat {
+		store.NewRentalBoat("Rubber Ring", 10, 1, false, false, "N/A", "N/A"),
+		store.NewRentalBoat("Yacht", 5000, 5, true, true, "Bob", "Alice"),
+		store.NewRentalBoat("Super Yacht", 100000, 15, true, true, "Dora", "Charlie"),
+	}
+
+	for _, r := range rentals {
+		fmt.Println("Rental Boat:", r.Name, "Rental Price:", r.Price(0.2), "Captain:", r.Captain)
+	}
+
+	product := store.NewProduct("Kayak", "Watersports", 279)
+
+	deal := store.NewSpecialDeal("Weekend Special", product, 50)
+
+	Name, price, Price := deal.GetDetails()
+
+	fmt.Println("Name:", Name)
+	fmt.Println("Price field:", price)
+	fmt.Println("Price method:", Price)
 }
